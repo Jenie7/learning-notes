5e862c12ba4e578f3c207a7a7b567d43ca5ac388
Jenie7 <mustafaalsayafi@gmail.com>
2022-10-23 05:32:09 +0300

    Update JavaScript Promises
    - Add parallel use for the same Promise
    - Add example of returning a custom Promise inside `then`

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,24 +1,30 @@
 const iceCreamOrder = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('Here is your ice cream!');
-    reject("I'm sorry, we are out of ice cream!");
-  }, 3000);
+  setTimeout(() => resolve('Here is your ice cream!'), 1500);
+  // setTimeout(() => reject("I'm sorry, we are out of ice cream!"), 2000);
 });
 
 iceCreamOrder
   .then((resolveValue) => {
-    console.log(iceCreamOrder);
+    // Promise { <fulfilled> }
     console.log(resolveValue);
-    return 'Have a good day!';
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve('\n\nHey wait!!\nYou forgot your phone!');
+      }, 3000)
+    );
   })
   .then((anotherValue) => {
     console.log(anotherValue);
   })
   .catch((rejectValue) => {
-    console.log(iceCreamOrder);
+    // Promise { <rejected> }
     console.error(rejectValue);
   });
 
-console.log(iceCreamOrder);
+// Promise { <pending> }
 console.log('Waiting..');
 console.log('Using your phone..');
+
+iceCreamOrder
+  .then(() => console.log('Thank you for visiting us!'))
+  .catch(() => console.error(':('));
