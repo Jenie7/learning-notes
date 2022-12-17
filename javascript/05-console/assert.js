950cc5a8b7beae140f80cb313fdca6ba8ed9473a
JenieX <sozorablack@gmail.com>
2022-12-17 06:34:04 +0300

    Cleanup

--- javascript/05-console/assert.js
+++ javascript/05-console/assert.js
@@ -1,5 +1,4 @@
-const errorMsg = 'the # is not even';
 for (let number = 2; number <= 5; number += 1) {
-  console.log(`the # is ${number}`);
-  console.assert(number % 2 === 0, '%o', { number, errorMsg });
+  console.log(number);
+  console.assert(number % 2 === 0);
 }
