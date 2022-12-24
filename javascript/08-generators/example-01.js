7abf73c69c007eb9ab44bf536b2f65b164ad8bbc
JenieX <sozorablack@gmail.com>
2022-12-24 14:40:02 +0300

    :white_check_mark: Add the first JavaScript generators notes

---
+++ javascript/08-generators/example-01.js
@@ -0,0 +1,42 @@
+const sleep = require('../../projects/assets/sleep');
+
+/**
+ * The function* declaration (function keyword followed by an asterisk)
+ * defines a generator function, which returns a Generator object.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
+ */
+
+function* counter() {
+  // Code in here will not run on this function call
+
+  console.log('x');
+  yield 1;
+
+  console.log('xx');
+  yield 2;
+
+  console.log('xxx');
+  yield 3;
+
+  console.log('End of generator');
+}
+
+console.log(counter);
+
+// Next line will not run the code inside that function
+const counterGenerator = counter();
+console.log(counterGenerator);
+
+(async () => {
+  console.log('');
+  console.log('Start');
+  console.log('');
+
+  for (let index = 1; index <= 8; index += 1) {
+    console.log(`#${index}`);
+    console.log(counterGenerator.next());
+    console.log('');
+    await sleep(3000);
+  }
+})();
